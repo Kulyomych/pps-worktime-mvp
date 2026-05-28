e3a2034048de2014114961ea4773d5bb3d207d51
Kulyomych <den.xrustalev17@yandex.ru>
2026-05-28 22:37:46 +0300

    deploy

--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -1,16 +1,26 @@
 import axios from "axios";
-import type { DepartmentReport, TeacherWorkloadListItem, TeacherWorkloadResponse, WorkloadRow } from "../types/workload";
+import type {
+  DepartmentReport,
+  TeacherWorkloadListItem,
+  TeacherWorkloadResponse,
+  WorkloadRow,
+} from "../types/workload";
 import type { ExcelDataSnapshot } from "../types/excel";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://amvera-Kulyomych17-teacherbackend.amvera.io",
 });
 
-export const saveTeacherWorkload = async (teacherId: string, rows: WorkloadRow[]) => {
+export const saveTeacherWorkload = async (
+  teacherId: string,
+  rows: WorkloadRow[],
+) => {
   await api.post("/workload", { teacherId, rows });
 };
 
-export const getTeacherWorkload = async (teacherId: string): Promise<TeacherWorkloadResponse> => {
+export const getTeacherWorkload = async (
+  teacherId: string,
+): Promise<TeacherWorkloadResponse> => {
   const response = await api.get(`/workload/${teacherId}`);
   return response.data;
 };
@@ -20,7 +30,9 @@ export const getDepartmentReport = async (): Promise<DepartmentReport> => {
   return response.data;
 };
 
-export const getAllTeacherWorkloads = async (): Promise<TeacherWorkloadListItem[]> => {
+export const getAllTeacherWorkloads = async (): Promise<
+  TeacherWorkloadListItem[]
+> => {
   const response = await api.get("/workload");
   return response.data;
 };
@@ -30,6 +42,8 @@ export const getExcelState = async (): Promise<ExcelDataSnapshot> => {
   return response.data.state;
 };
 
-export const saveExcelState = async (state: ExcelDataSnapshot): Promise<void> => {
+export const saveExcelState = async (
+  state: ExcelDataSnapshot,
+): Promise<void> => {
   await api.put("/state", { state });
 };
